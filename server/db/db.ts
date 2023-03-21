0c9d1763de0dd3f45ac763952bbd349a3beac9f6
Joshowens16 <joshowens16@gmail.com>
2023-03-20 18:26:59 -0700

    revert

--- server/db/db.ts
+++ server/db/db.ts
@@ -2,8 +2,9 @@ import { Sequelize } from 'sequelize';
 const config = {
   logging: false,
 };
-const railway = process.env.DATABASE_URL;
+const DB_NAME = 'codenames';
+const URL = `postgres://localhost/${DB_NAME}`;
 
-const db = new Sequelize(railway, config);
+const db = new Sequelize(process.env.DATABASE_URL || URL, config);
 
 export default db;
